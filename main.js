3e4885f32e7e71201697fe23e4c1285695880fbb
safi-0412 <omonjanovas@gmail.com>
2025-01-29 02:10:41 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+// function bot (){
+//     const userName = prompt("Ism kiriting")
+//     console.log(userName);
+//     const newTeg = document.createElement("h1");
+//     newTeg.textContent = userName;
+//     document.body.appendChild(newTeg);
+// }
+// bot();
+
+function enableLightMode() {
+  document.body.classList.remove('dark-mode');
+}
+
+function enableDarkMode() {
+  document.body.classList.add('dark-mode');
+}
